587bcb2d5ec6e3b6f06231936dc92d8e9129f45f
Nils Gudmundsson <nils.gudmundsson@dynabyte.se>
2013-10-14 19:46:04 +0200

    intial commit

---
+++ DynaShipServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Bend.Util;
+
+namespace ConsoleApplication1
+{
+    public class DynaShipServer : HttpServer
+    {
+        public DynaShipServer(int port)
+            : base(port)
+        {
+        }
+
+        public override void handleGETRequest(HttpProcessor p)
+        {
+            p.outputStream.Write("Nooooo........!");
+        }
+
+        public override void handlePOSTRequest(HttpProcessor p, StreamReader inputData)
+        {
+            Console.WriteLine("POST request: {0}", p.http_url);
+            string data = inputData.ReadToEnd();
+
+            p.outputStream.Write("{x: 5, y: 2}");
+        }
+    }
+}
